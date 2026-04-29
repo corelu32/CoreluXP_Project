25df06f0d959daef661679fcb68b6cc67e0b5161
Luke Bates <lnbates20@gmail.com>
2026-04-29 15:01:48 -0400

    Made the ApplicationWindow's internal handle readonly.

--- Lu3DX/src/ApplicationWindow.cs
+++ Lu3DX/src/ApplicationWindow.cs
@@ -2,121 +2,118 @@ using static SDL.SDL3;
 using SDL;
 using Lu3DX.Primitives;
 
-namespace Lu3DX;
-
-
-
-/// <summary>
-///   Options to initialize an application window.
-/// </summary>
-
-public struct ApplicationWindowOptions
+namespace Lu3DX
 {
-    public required string Title;
-    public required int    Width;
-    public required int    Height;
-
-    public ApplicationWindowOptions() { }
-}
-
-
-
-/// <summary>
-///   An application window for rendering graphics and event handling.
-/// </summary>
-
-public unsafe sealed class ApplicationWindow
-{
-    private SDL_Window* _window = null;
-
-    public ApplicationWindowOptions Options { get; private set; }
-    public bool                     Running { get; private set; }
-
-    public Action?          OnStart   { private get; set; }
-    public Action?          OnQuit    { private get; set; }
-    public Action<KeyCode>? OnKeyDown { private get; set; }
-    public Action<float>?   OnUpdate  { private get; set; }
-
     /// <summary>
-    ///   Initializes SDL and creates the window.
+    ///   Options to initialize an application window.
     /// </summary>
 
-    public ApplicationWindow(ApplicationWindowOptions options)
+    public struct ApplicationWindowOptions
     {
-        Options = options;
-        Running = true;
+        public required string Title;
+        public required int Width;
+        public required int Height;
 
-        // Initialize SDL. Ensure it doesn't fail.
-        if (!SDL_Init(SDL_InitFlags.SDL_INIT_VIDEO))
-            throw new ApplicationException($"Failed to initialize SDL3. {SDL_GetError()}");
-        
-        // Create the window handle. Ensure it doesn't fail.
-        //   - Hide the window until the Run() method is called.
-        //   - This prevents an "unprepared" window from showing briefly during startup.
-        _window = SDL_CreateWindow(
-            options.Title,
-            options.Width,
-            options.Height,
-            SDL_WindowFlags.SDL_WINDOW_HIDDEN);
-        
-        if (_window is null)
-            throw new ApplicationException($"Failed to create the window. {SDL_GetError()}");
+        public ApplicationWindowOptions() { }
     }
 
     /// <summary>
-    ///   Run the update loop.
+    ///   An application window for rendering graphics and event handling.
     /// </summary>
 
-    public void Run()
+    public unsafe sealed class ApplicationWindow
     {
-        OnStart?.Invoke();
-        SDL_ShowWindow(_window);
+        private readonly SDL_Window* _window = null;
 
-        while (Running)
+        public ApplicationWindowOptions Options { get; private set; }
+        public bool Running { get; private set; }
+
+        public Action? OnStart { private get; set; }
+        public Action? OnQuit { private get; set; }
+        public Action<KeyCode>? OnKeyDown { private get; set; }
+        public Action<float>? OnUpdate { private get; set; }
+
+        /// <summary>
+        ///   Initializes SDL and creates the window.
+        /// </summary>
+
+        public ApplicationWindow(ApplicationWindowOptions options)
         {
-            PollEvents();
-            OnUpdate?.Invoke(0);
+            Options = options;
+            Running = true;
+
+            // Initialize SDL. Ensure it doesn't fail.
+            if (!SDL_Init(SDL_InitFlags.SDL_INIT_VIDEO))
+                throw new ApplicationException($"Failed to initialize SDL3. {SDL_GetError()}");
+
+            // Create the window handle. Ensure it doesn't fail.
+            //   - Hide the window until the Run() method is called.
+            //   - This prevents an "unprepared" window from showing briefly during startup.
+            _window = SDL_CreateWindow(
+                options.Title,
+                options.Width,
+                options.Height,
+                SDL_WindowFlags.SDL_WINDOW_HIDDEN);
+
+            if (_window is null)
+                throw new ApplicationException($"Failed to create the window. {SDL_GetError()}");
         }
 
-        OnQuit?.Invoke();
-        SDL_DestroyWindow(_window);
-        SDL_Quit();
-    }
+        /// <summary>
+        ///   Run the update loop.
+        /// </summary>
 
-    /// <summary>
-    ///   Stop the update loop as soon as possible.
-    /// </summary>
+        public void Run()
+        {
+            OnStart?.Invoke();
+            SDL_ShowWindow(_window);
 
-    public void Stop()
-    {
-        Running = false;
-    }
-
-    /// <summary>
-    ///   Poll queued window events.
-    /// </summary>
-
-    private void PollEvents()
-    {
-        SDL_Event e;
-
-        // `SDL_Event` objects are queued during runtime.
-        // `SDL_PollEvent(e)` will dequeue each event into `e` until none are left.
-
-        // Each `SDL_EventType` is responsible for executing the relevant
-        // user-defined delegate.
-
-        while (SDL_PollEvent(&e))
-            switch ((SDL_EventType)e.type)
+            while (Running)
             {
-                case SDL_EventType.SDL_EVENT_QUIT:
-                    OnQuit?.Invoke();
-                    Stop();
-                    break;
-                
-                case SDL_EventType.SDL_EVENT_KEY_DOWN:
-                    OnKeyDown?.Invoke((KeyCode) e.key.key);
-                    break;
+                PollEvents();
+                OnUpdate?.Invoke(0);
             }
+
+            OnQuit?.Invoke();
+            SDL_DestroyWindow(_window);
+            SDL_Quit();
+        }
+
+        /// <summary>
+        ///   Stop the update loop as soon as possible.
+        /// </summary>
+
+        public void Stop()
+        {
+            Running = false;
+        }
+
+        /// <summary>
+        ///   Poll queued window events.
+        /// </summary>
+
+        private void PollEvents()
+        {
+            SDL_Event e;
+
+            // `SDL_Event` objects are queued during runtime.
+            // `SDL_PollEvent(e)` will dequeue each event into `e` until none are left.
+
+            // Each `SDL_EventType` is responsible for executing the relevant
+            // user-defined delegate.
+
+            while (SDL_PollEvent(&e))
+                switch ((SDL_EventType)e.type)
+                {
+                    case SDL_EventType.SDL_EVENT_QUIT:
+                        OnQuit?.Invoke();
+                        Stop();
+                        break;
+
+                    case SDL_EventType.SDL_EVENT_KEY_DOWN:
+                        OnKeyDown?.Invoke((KeyCode)e.key.key);
+                        break;
+                }
+        }
     }
 }
