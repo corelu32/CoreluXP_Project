a42d9d3bb7fa5dcefe87acb6d0cec4e38369f959
Luke Bates <lnbates20@gmail.com>
2026-05-16 02:44:50 -0400

    gen_sdl_bindings updates

--- gen_sdl_bindings.py
+++ gen_sdl_bindings.py
@@ -23,6 +23,12 @@ def build_repo(repo_path: Path, extra_flags: str = "") -> None:
     os_command(f"cmake -S {repo_path} -B {build_dir} -G Ninja -DCMAKE_BUILD_TYPE=Release {extra_flags}")
     os_command(f"cmake --build {build_dir}")
 
+def generate_bindings(args: dict[str, str]) -> None:
+    args_joined = " ".join(f"{flag} {value}" for (flag, value) in args.items())
+    
+    print(f"Running: ClangSharpPInvokeGenerator {args_joined}")
+    os_command(f"ClangSharpPInvokeGenerator {args_joined}")
+
 def main() -> None:
     print("PULLING GITHUB UPDATES...")
     sdl_path         = update_repo("https://github.com/libsdl-org/SDL.git")
@@ -34,9 +40,31 @@ def main() -> None:
     print("BUILDING NATIVE BINARIES...")
     build_repo(sdl_path)
     
-    # Configure and build SDL_shadercross using paths derived directly from Path objects
     sdl3_build_dir = sdl_path / "build"
     build_repo(shadercross_path, extra_flags=f"-DSDLSHADERCROSS_VENDORED=ON -DSDL3_DIR={sdl3_build_dir}")
+
+    print("GENERATING CORE SDL3 BINDINGS...")
+    generate_bindings({
+        "--file"            : f'"{sdl_path / "include/SDL3/SDL.h"}"',
+        "--output"          : f'"{PROJECT_DIR / "SDL3" / "SDL3.cs"}"',
+        "--namespace"       : "CoreluXP.SDL3",
+        "--libraryPath"     : "SDL3",
+        "--methodClassName" : "SDL3Api",
+        "--config"          : "latest-codegen generate-macro-bindings",
+        "--std"             : "c11"
+    })
+
+    print("GENERATING SHADERCROSS BINDINGS...")
+    generate_bindings({
+        "--file"            : f'"{shadercross_path / "include/SDL3_shadercross/SDL_shadercross.h"}"',
+        "--output"          : f'"{PROJECT_DIR / "SDL3" / "SDL3ShaderCross.cs"}"',
+        "--namespace"       : "CoreluXP.SDL3",
+        "--libraryPath"     : "SDL3_Shadercross",
+        "--methodClassName" : "ShaderCrossApi",
+        "--config"          : "latest-codegen generate-macro-bindings",
+        "--std"             : "c11",
+        "-a"                : f'"-I{sdl_path}/include"'
+    })
 
 if __name__ == "__main__":
     main()
